6cfabc1aac240213381ffa838dce8d6b5e743739
Nachiketa bhattacharya <nb5542@srmist.edu.in>
2026-06-20 02:51:18 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.76%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def summaryRanges(self, nums):
+        ans = []
+        i = 0
+        while i < len(nums):
+            start = nums[i]
+            while i + 1 < len(nums) and nums[i+1] == nums[i] + 1:
+                  i += 1
+
+        
+           
+        
+
+            end = nums[i]
+
+            if start == end:
+                ans.append(str(start))
+
+        
+            else:
+                 ans.append(str(start) + "->" + str(end))
+        
+
+            i += 1
+
+        return ans
+
+    
+
+    
+
+
+       
+        
