77e2121eb1d109d9d29fabf880dca5857dcde889
Nachiketa bhattacharya <nb5542@srmist.edu.in>
2026-06-29 13:03:47 +0530

    Time: 0 ms (100%), Space: 12.2 MB (89.19%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,15 +1,9 @@
 class Solution(object):
     def addDigits(self, num):
-
-        while num >= 10:
-
-            digit_sum = 0
-
-            while num > 0:
-                digit_sum += num % 10
-                num //= 10
-
-            num = digit_sum
-
-        return num
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        return num % 9
         
+
