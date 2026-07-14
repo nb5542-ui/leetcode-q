2f77fb801c3e96f5ef74921196d272586afa1fe5
Nachiketa bhattacharya <nb5542@srmist.edu.in>
2026-07-14 18:35:00 +0530

    Time: 4 ms (46.83%), Space: 12.2 MB (94.31%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        freq = {}
+        for ch in s:
+            freq[ch] = freq.get(ch,0) + 1
+        length = 0
+        odd_found = False
+        for count in freq.values():
+            if count%2 == 0:
+                length += count
+            else:
+                length += count-1
+                odd_found = True
+
+        if odd_found:
+            length += 1
+
+        return length
+
+            
+
+
+        
+        
