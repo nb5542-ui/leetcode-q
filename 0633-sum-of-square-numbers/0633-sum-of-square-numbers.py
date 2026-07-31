01f3721e174a51ee3b449aaeace94429217b001b
Nachiketa bhattacharya <nb5542@srmist.edu.in>
2026-07-31 22:27:10 +0530

    Time: 41 ms (95.36%), Space: 12.4 MB (32.01%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def judgeSquareSum(self, c):
+
+        left = 0
+        right = int(c ** 0.5)
+
+        while left <= right:
+
+            total = left * left + right * right
+
+            if total == c:
+                return True
+
+            elif total < c:
+                left += 1
+
+            else:
+                right -= 1
+
+        return False
+             
+        
