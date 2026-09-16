84aa9531f69292cb643f363cfe222aefcd750fb8
Nachiketa bhattacharya <nb5542@srmist.edu.in>
2026-09-16 21:31:51 +0530

    Time: 21 ms (51.79%), Space: 14.2 MB (40.82%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def distributeCandies(self, candyType):
+        unique = len(set(candyType))
+        allowed = len(candyType) // 2
+
+        return min(unique,allowed)
+        
+
+        
+        
+        
